c03cdc773b1d2d566121626fe5f0aacdec2e3f2b
Yaswanth Kancharla <yaswanth.kancharla65@gmail.com>
2026-05-21 11:22:21 +0530

    Fix deps: tailwindcss v3, add .npmrc, update metro config

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,20 @@
 const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro");
+const path = require("path");
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: "./global.css" });
+try {
+  const nativewindMetro = require(path.join(
+    __dirname,
+    "node_modules",
+    "nativewind",
+    "dist",
+    "metro",
+    "index.js"
+  ));
+  module.exports = nativewindMetro.withNativeWind(config, {
+    input: "./global.css",
+  });
+} catch (e) {
+  module.exports = config;
+}
